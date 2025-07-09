aaaf937ef1d4ae53d25c35504632df3fb4b95cdb
glitchyfishys <glitchyfishys@gmail.com>
2025-07-09 17:10:57 +1200

    why?

--- src/core/secret-formula/tabs.js
+++ src/core/secret-formula/tabs.js
@@ -556,7 +556,7 @@ export const tabs = [
     UIClass: "o-tab-btn--absurdity",
     hideAt: 2.3,
     id: 12,
-    condition: () => PlayerProgress.realityUnlocked() || PlayerProgress.absurdityUnlocked(),
+    condition: () => PlayerProgress.realityUnlocked() || PlayerProgress.absurdityUnlocked(), // i think it borked
     hidable: true,
     subtabs: [
       {
